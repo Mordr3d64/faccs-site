72c44aa8e29ee209e227b1e2eed7065b128e9406
CharlesPetisme <charles_petisme@dlsu.edu.ph>
2025-03-14 16:27:18 +0800

    Deleted Extra Lines

--- src/App.js
+++ src/App.js
@@ -4,18 +4,6 @@ import './App.css';
 import React, { useRef, useState, useEffect } from 'react';
 import emailjs from '@emailjs/browser';
 
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 function App() {
   return (
     <Router>
