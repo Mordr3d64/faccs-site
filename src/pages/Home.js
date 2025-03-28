7166dc21fc556228060dfbfa514f292be7a2bba0
MLG1725 <martin_liam_go@dlsu.edu.ph>
2025-03-28 22:59:34 +0800

    Home Photo Carousel

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import image1 from '../assets/images/image1.jpg'; // Import images
+import image2 from '../assets/images/image2.jpg';
+import image3 from '../assets/images/image3.jpg';
+import image3 from '../assets/images/image4.jpg';
 
 function Home() {
   return (
@@ -13,12 +17,24 @@ function Home() {
             </p>
           </div>
         </div>
-        <div className="home-image">
-          <img src="https://placehold.co/600x400" alt="FACCS" />
+
+        {/* Photo Carousel Section */}
+        <div className="home-carousel">
+          <Carousel>
+            <div>
+              <img src={image1} alt="Image 1" />
+            </div>
+            <div>
+              <img src={image2} alt="Image 2" />
+            </div>
+            <div>
+              <img src={image3} alt="Image 3" />
+            </div>
+          </Carousel>
         </div>
       </div>
     </div>
   );
 }
 
-export default Home; 
+export default Home;
