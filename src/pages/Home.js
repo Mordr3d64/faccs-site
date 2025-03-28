7ca25e7e0e18e94b846493bfb3fae0fb7f04dfd9
MLG1725 <martin_liam_go@dlsu.edu.ph>
2025-03-28 23:01:56 +0800

    try

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import image1 from '../assets/images/image1.jpg'; // Import images
 import image2 from '../assets/images/image2.jpg';
 import image3 from '../assets/images/image3.jpg';
-import image3 from '../assets/images/image4.jpg';
+import image4 from '../assets/images/image4.jpg';
 
 function Home() {
   return (
